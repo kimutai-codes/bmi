af241e05845fa0df71371476f1455d521e1373f7
kimutai-codes <allankimutai@gmail.com>
2021-03-19 15:36:28 +0300

    pili

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+const App = () => {
+  return (
+    <AppWrapper>
+      <h1>Blaa</h1>
+    </AppWrapper>
+  );
+};
+export default App;
